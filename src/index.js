680da7d9689f63c7b5690aba30d368ff726cb495
Tania Tatula <tgrogol@gmail.com>
2021-04-07 22:58:20 +0300

    форматує файл

--- src/index.js
+++ src/index.js
@@ -1,33 +1,29 @@
-import {colors} from './colors.js';
+import { colors } from "./colors.js";
 
 const startBtn = document.querySelector('[data-action="start"]');
 const stopBtn = document.querySelector('[data-action="stop"]');
-const body = document.querySelector('body');
+const body = document.querySelector("body");
 // console.log(colors.length);
 let timerId = null;
 
-startBtn.addEventListener('click', changesColorBody);
+startBtn.addEventListener("click", changesColorBody);
 stopBtn.addEventListener("click", stopChangesColorBody);
 
 function changesColorBody() {
-    timerId = setInterval(() => {
-    
+  timerId = setInterval(() => {
     const nubberColor = randomIntegerFromInterval(0, colors.length - 1);
 
     body.style.backgroundColor = colors[nubberColor];
-    }, 1000);
+  }, 1000);
 
-    startBtn.removeEventListener('click', changesColorBody);
-};
+  startBtn.removeEventListener("click", changesColorBody);
+}
 
 const randomIntegerFromInterval = (min, max) => {
-    return Math.floor(Math.random() * (max - min + 1) + min);
-  };
+  return Math.floor(Math.random() * (max - min + 1) + min);
+};
 
-
- function stopChangesColorBody(){
-      clearInterval(timerId);
-      startBtn.addEventListener('click', changesColorBody);
-    };
-
-
+function stopChangesColorBody() {
+  clearInterval(timerId);
+  startBtn.addEventListener("click", changesColorBody);
+}
